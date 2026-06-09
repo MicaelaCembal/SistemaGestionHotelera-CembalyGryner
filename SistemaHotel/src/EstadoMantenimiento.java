9dfc2391232a34bbcf69e85bf52cf368de653fc1
Micaela Cembal <micaelacembal@gmail.com>
2026-06-09 17:49:08 -0300

    Creacion EstadoMantenimiento, EstadoLimpieza, EstadoOcupado

---
+++ SistemaHotel/src/EstadoMantenimiento.java
@@ -0,0 +1,22 @@
+public class EstadoMantenimiento implements EstadoHabitacion {
+
+    @Override
+    public void ocupar() {
+
+    }
+
+    @Override
+    public void liberar() {
+
+    }
+
+    @Override
+    public void iniciarLimpieza() {
+
+    }
+
+    @Override
+    public void iniciarMantenimiento() {
+
+    }
+}
