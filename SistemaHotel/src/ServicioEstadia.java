50a4991c7b5d99f5133eb2c6c86d07051b466915
Micaela Cembal <micaelacembal@gmail.com>
2026-06-17 10:02:40 -0300

    constructor, getters y setters en servicio estadia y tipo de habitacion

--- SistemaHotel/src/ServicioEstadia.java
+++ SistemaHotel/src/ServicioEstadia.java
@@ -9,4 +9,73 @@ public class ServicioEstadia {
     private double subtotal;
     private IServicio servicio;
 
+    public double calcularSubtotal(){
+        return 0;
+    }
+
+    public ServicioEstadia(int idServicioEstadia, int idEstadia, int idServicio, int cantidad, LocalDateTime fecha, double subtotal, IServicio servicio) {
+        this.idServicioEstadia = idServicioEstadia;
+        this.idEstadia = idEstadia;
+        this.idServicio = idServicio;
+        this.cantidad = cantidad;
+        this.fecha = fecha;
+        this.subtotal = subtotal;
+        this.servicio = servicio;
+    }
+
+    public int getIdServicioEstadia() {
+        return idServicioEstadia;
+    }
+
+    public void setIdServicioEstadia(int idServicioEstadia) {
+        this.idServicioEstadia = idServicioEstadia;
+    }
+
+    public int getIdEstadia() {
+        return idEstadia;
+    }
+
+    public void setIdEstadia(int idEstadia) {
+        this.idEstadia = idEstadia;
+    }
+
+    public int getIdServicio() {
+        return idServicio;
+    }
+
+    public void setIdServicio(int idServicio) {
+        this.idServicio = idServicio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public IServicio getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(IServicio servicio) {
+        this.servicio = servicio;
+    }
 }
--- SistemaHotel/src/TipoHabitacion.java
+++ SistemaHotel/src/TipoHabitacion.java
@@ -4,8 +4,44 @@ public class TipoHabitacion {
     private int capacidad;
     private double precioBaseNoche;
 
+    public int getIdTipoHabitacion() {
+        return idTipoHabitacion;
+    }
+
+    public void setIdTipoHabitacion(int idTipoHabitacion) {
+        this.idTipoHabitacion = idTipoHabitacion;
+    }
+
+    public TipoHabitacion(int idTipoHabitacion) {
+        this.idTipoHabitacion = idTipoHabitacion;
+    }
+
     public double verPrecioBase(){
         return precioBaseNoche;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public double getPrecioBaseNoche() {
+        return precioBaseNoche;
+    }
+
+    public void setPrecioBaseNoche(double precioBaseNoche) {
+        this.precioBaseNoche = precioBaseNoche;
     }
 
     public int verCapacidad(){
