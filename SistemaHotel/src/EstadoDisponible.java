c4e157d4b84fa42fd69e448917f317abde9d493c
Agustina Gryner <agustinagryner@gmail.com>
2026-06-19 18:54:17 -0300

    Implementacion de clases de estado de habitaciones.

--- SistemaHotel/src/EstadoDisponible.java
+++ SistemaHotel/src/EstadoDisponible.java
@@ -2,21 +2,21 @@ public class EstadoDisponible implements EstadoHabitacion {
 
     @Override
     public void ocupar() {
-
+        System.out.println("La habitación pasó a estado ocupado.");
     }
 
     @Override
     public void liberar() {
-
+        System.out.println("La habitación ya está disponible.");
     }
 
     @Override
     public void iniciarLimpieza() {
-
+        System.out.println("La habitación pasó a limpieza.");
     }
 
     @Override
     public void iniciarMantenimiento() {
-
+        System.out.println("La habitación pasó a mantenimiento.");
     }
 }
