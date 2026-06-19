a0a1db7ddb2de12625b224894398afea95cf50c9
Agustina Gryner <agustinagryner@gmail.com>
2026-06-19 17:45:47 -0300

    Getters y setters que faltaban

--- SistemaHotel/src/Promocion.java
+++ SistemaHotel/src/Promocion.java
@@ -17,4 +17,51 @@ public class Promocion {
         // Implementación
         return 0.0;
     }
+    public int getIdPromocion() {
+        return idPromocion;
+    }
+
+    public void setIdPromocion(int id) {
+        this.idPromocion = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String n) {
+        this.nombre = n;
+    }
+
+    public double getPorcentajeDescuento() {
+        return porcentajeDescuento;
+    }
+
+    public void setPorcentajeDescuento(double p) {
+        this.porcentajeDescuento = p;
+    }
+
+    public LocalDateTime getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDateTime f) {
+        this.fechaInicio = f;
+    }
+
+    public LocalDateTime getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDateTime f) {
+        this.fechaFin = f;
+    }
+
+    public TipoPromocion getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoPromocion t) {
+        this.tipo = t;
+    }
 }
